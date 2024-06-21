c733857ff8fe90c0c9d582d9453b87e1eee06729
Gordon Smith <iamgordonsmith@icloud.com>
2024-06-21 11:36:23 +0100

    feat: create base xcode project

---
+++ BoilerplateApp/BoilerplateApp/BoilerplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerplateAppApp.swift
+//  BoilerplateApp
+//
+//  Created by Gordon on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoilerplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
