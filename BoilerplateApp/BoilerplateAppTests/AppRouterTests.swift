af5e67a8dbc1091e16b6b623f5089fa89183a132
Gordon Smith <iamgordonsmith@icloud.com>
2024-06-21 19:30:08 +0100

    test: navigateTo appends route to path

--- BoilerplateApp/BoilerplateAppTests/AppRouterTests.swift
+++ BoilerplateApp/BoilerplateAppTests/AppRouterTests.swift
@@ -9,22 +9,33 @@ import XCTest
 import SwiftUI
 
 class AppRouter {
-    var path: NavigationPath
+    var path: [AnyHashable]
 
-    init(with path: NavigationPath) {
+    init(with path: [AnyHashable]) {
         self.path = path
     }
+    
+    func navigate(to destination: AnyHashable) {
+        path.append(destination)
+    }
 }
-
 
 final class AppRouterTests: XCTestCase {
 
     func test_init_hasNoSideEffects() {
-        let stub = NavigationPath()
-        XCTAssertTrue(stub.isEmpty)
+        let requests = [AnyHashable]()
+        XCTAssertTrue(requests.isEmpty)
         
-        _ = AppRouter(with: stub)
-        XCTAssertTrue(stub.isEmpty)
+        let sut = AppRouter(with: requests)
+        XCTAssertTrue(sut.path.isEmpty)
     }
-
+    
+    func test_navigateTo_appendsRouteToPath() {
+        let sut = AppRouter(with: [UUID]())
+        
+        let route = UUID()
+        sut.navigate(to: route)
+        
+        XCTAssertEqual(sut.path, [route])
+    }
 }
