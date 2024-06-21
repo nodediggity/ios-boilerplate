2797db4100f6d275073366afde7593428ca53bf5
Gordon Smith <iamgordonsmith@icloud.com>
2024-06-21 22:49:46 +0100

    chore: move `HTTPClient` to own file

---
+++ BoilerplateApp/BoilerplateApp/Modules/Network/Feature/HTTPClient.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPClient.swift
+//  BoilerplateApp
+//
+//  Created by Gordon on 21/06/2024.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    typealias Resource = (data: Data, response: HTTPURLResponse)
+    /// This method can be invoked in any thread.
+    /// Clients are responsible for dispatch to appropriate threads, if needed.
+    func dispatch(_ request: URLRequest) async throws -> Resource
+}
