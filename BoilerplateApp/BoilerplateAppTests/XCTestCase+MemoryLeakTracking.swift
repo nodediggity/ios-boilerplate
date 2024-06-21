82c2842da8a19f0ea677220677d88cdd2f2cb028
Gordon Smith <iamgordonsmith@icloud.com>
2024-06-21 19:49:10 +0100

    test: add `trackForMemoryLeaks` test helper

---
+++ BoilerplateApp/BoilerplateAppTests/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  BoilerplateAppTests
+//
+//  Created by Gordon on 21/06/2024.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
