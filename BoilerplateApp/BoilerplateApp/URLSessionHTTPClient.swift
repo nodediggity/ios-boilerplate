ca488d8ae75bf97b607d9e91483758d6cca0024a
Gordon Smith <iamgordonsmith@icloud.com>
2024-06-21 22:39:27 +0100

    refactor: define `HTTPClient` abstract interface

--- BoilerplateApp/BoilerplateApp/URLSessionHTTPClient.swift
+++ BoilerplateApp/BoilerplateApp/URLSessionHTTPClient.swift
@@ -7,8 +7,15 @@
 
 import Foundation
 
-public final class URLSessionHTTPClient {
-    public typealias Resource = (data: Data, response: HTTPURLResponse)
+public protocol HTTPClient {
+    typealias Resource = (data: Data, response: HTTPURLResponse)
+    /// This method can be invoked in any thread.
+    /// Clients are responsible for dispatch to appropriate threads, if needed.
+    func dispatch(_ request: URLRequest) async throws -> Resource
+}
+
+public final class URLSessionHTTPClient: HTTPClient {
+    public typealias Resource = HTTPClient.Resource
     private let session: URLSession
 
     public init(session: URLSession) {
