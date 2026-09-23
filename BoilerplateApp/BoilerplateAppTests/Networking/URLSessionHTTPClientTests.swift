fd4fc6ba0169a211711600ec0a542b373c4b5f8d
nodediggity <iamgordonsmith@icloud.com>
2026-09-23 18:40:14 +0100

    test(httpclient): use Swift Testing for `URLSessionHTTPClientTests` assertions

--- BoilerplateApp/BoilerplateAppTests/Networking/URLSessionHTTPClientTests.swift
+++ BoilerplateApp/BoilerplateAppTests/Networking/URLSessionHTTPClientTests.swift
@@ -2,139 +2,178 @@
 // Created 21/06/2024.
 
 import BoilerplateApp
-import XCTest
+import Foundation
+import Testing
 
-final class URLSessionHTTPClientTests: XCTestCase {
-    override func tearDown() {
-        super.tearDown()
-        URLProtocolStub.removeStub()
+@Suite("URL Session HTTP Client", .serialized)
+struct URLSessionHTTPClientTests {
+    
+    @Test("Dispatch delivers error on request failure")
+    func dispatchDeliversErrorOnRequestFailure() async {
+        let fixtures = TestFixtures()
+        
+        let error = Test.makeError()
+        let receivedError = await fixtures.resultErrorFor(data: nil, response: nil, error: error)
+        
+        #expect(receivedError != nil)
     }
-
-    func test_dispatch_deliversErrorOnRequestFailure() async {
-        let error = makeError()
-        let receivedError = await resultErrorFor(data: nil, response: nil, error: error)
-        XCTAssertNotNil(receivedError)
-    }
-
-    func test_dispatch_deliversErrorOnInvalidRepresentationCases() async {
+    
+    @Test("Dispatch delivers error on invalid representation cases")
+    func dispatchDeliversErrorOnInvalidRepresentationCases() async {
         /*
-           These cases should *never* happen, however as `URLSession` represents these fields as optional
-           it is possible in some obscure way that this state _could_ exist.
-
-           | Data?    | URLResponse?      | Error?   |
-           |----------|-------------------|----------|
-           | nil      | URLResponse       | nil      |
-           | value    | nil               | value    |
-           | nil      | URLResponse       | value    |
-           | nil      | HTTPURLResponse   | value    |
-           | value    | HTTPURLResponse   | value    |
-           | value    | URLResponse       | nil      |
+         These cases should *never* happen, however as `URLSession` represents these fields as optional
+         it is possible in some obscure way that this state _could_ exist.
+         
+         | Data?    | URLResponse?      | Error?   |
+         |----------|-------------------|----------|
+         | nil      | URLResponse       | nil      |
+         | value    | nil               | value    |
+         | nil      | URLResponse       | value    |
+         | nil      | HTTPURLResponse   | value    |
+         | value    | HTTPURLResponse   | value    |
+         | value    | URLResponse       | nil      |
          */
-
-        let nonHTTPURLResponse = URLResponse(url: makeURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
-        let httpResponse = HTTPURLResponse(url: makeURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
-        let data = makeData()
-        let error = makeError()
-
-        let requests = await [
-            resultErrorFor(data: nil, response: nonHTTPURLResponse, error: nil),
-            resultErrorFor(data: data, response: nil, error: error),
-            resultErrorFor(data: nil, response: nonHTTPURLResponse, error: error),
-            resultErrorFor(data: nil, response: httpResponse, error: error),
-            resultErrorFor(data: data, response: nonHTTPURLResponse, error: error),
-            resultErrorFor(data: data, response: httpResponse, error: error),
-            resultErrorFor(data: data, response: nonHTTPURLResponse, error: nil)
+        
+        let fixtures = TestFixtures()
+        
+        let nonHTTPURLResponse = URLResponse(url: Test.makeURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        let httpResponse = HTTPURLResponse(url: Test.makeURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+        
+        let data = Test.makeData()
+        let error = Test.makeError()
+        
+        let errors = [
+            await fixtures.resultErrorFor(data: nil, response: nonHTTPURLResponse, error: nil),
+            await fixtures.resultErrorFor(data: data, response: nil, error: error),
+            await fixtures.resultErrorFor(data: nil, response: nonHTTPURLResponse, error: error),
+            await fixtures.resultErrorFor(data: nil, response: httpResponse, error: error),
+            await fixtures.resultErrorFor(data: data, response: nonHTTPURLResponse, error: error),
+            await fixtures.resultErrorFor(data: data, response: httpResponse, error: error),
+            await fixtures.resultErrorFor(data: data, response: nonHTTPURLResponse, error: nil)
         ]
-
-        for (index, element) in requests.enumerated() {
-            XCTAssertNotNil(element, "Expected error for item at \(index) but got nil instead")
+        
+        for error in errors {
+            #expect(error != nil)
         }
     }
-
-    func test_dispatch_performsRequestWithGivenObject() async throws {
+    
+    @Test("Dispatch performs request with given object")
+    func dispatchPerformsRequestWithGivenObject() async throws {
+        let fixtures = TestFixtures()
+        let sut = fixtures.makeSUT()
+        
         let requestID = UUID().uuidString
-        let sut = makeSUT()
-
-        let requestURL = makeURL()
+        let requestURL = Test.makeURL()
+        
         var request = URLRequest(url: requestURL)
         request.addValue(requestID, forHTTPHeaderField: "REQUEST_ID")
-
+        
         let httpResponse = HTTPURLResponse(url: requestURL, statusCode: 200, httpVersion: nil, headerFields: nil)
-        let data = makeData()
-
-        URLProtocolStub.stub(data: data, response: httpResponse, error: .none)
-
+        
+        let data = Test.makeData()
+        
+        URLProtocolStub.stub(data: data, response: httpResponse, error: nil)
+        
         _ = try await sut.dispatch(request)
-
-        XCTAssertEqual(URLProtocolStub.observedRequests.count, 1)
-        expectRequest(at: 0, toEqual: request)
+        
+        #expect(URLProtocolStub.observedRequests.count == 1)
+        fixtures.expectRequest(at: 0, toEqual: request)
     }
-
-    func test_dispatch_deliversSuccessOnHTTPURLResponseWithData() async {
-        let data = makeData()
-        let httpResponse = HTTPURLResponse(url: makeURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
-        let output = await resultValuesFor(data: data, response: httpResponse, error: nil)
-
-        XCTAssertEqual(output?.data, data)
-        XCTAssertEqual(output?.response.url, httpResponse?.url)
-        XCTAssertEqual(output?.response.statusCode, httpResponse?.statusCode)
+    
+    @Test("Dispatch delivers success on HTTPURLResponse with data")
+    func dispatchDeliversSuccessOnHTTPURLResponseWithData() async {
+        let fixtures = TestFixtures()
+        
+        let data = Test.makeData()
+        let httpResponse = HTTPURLResponse(url: Test.makeURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+        
+        let output = await fixtures.resultValuesFor(data: data, response: httpResponse, error: nil)
+        
+        #expect(output?.data == data)
+        #expect(output?.response.url == httpResponse?.url)
+        #expect(output?.response.statusCode == httpResponse?.statusCode)
     }
-
-    func test_dispatch_deliversSuccessWithEmptyDataOnHTTPURLResponseWithMissingData() async {
-        let emptyData = makeData()
-        let httpResponse = HTTPURLResponse(url: makeURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
-        let output = await resultValuesFor(data: nil, response: httpResponse, error: nil)
-
-        XCTAssertEqual(output?.data, emptyData)
-        XCTAssertEqual(output?.response.url, httpResponse?.url)
-        XCTAssertEqual(output?.response.statusCode, httpResponse?.statusCode)
+    
+    @Test("Dispatch delivers success with empty data on HTTPURLResponse with missing data")
+    func dispatchDeliversSuccessWithEmptyDataOnHTTPURLResponseWithMissingData() async {
+        let fixtures = TestFixtures()
+        
+        let emptyData = Test.makeData()
+        let httpResponse = HTTPURLResponse(url: Test.makeURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+        
+        let output = await fixtures.resultValuesFor(data: nil, response: httpResponse, error: nil)
+        
+        #expect(output?.data == emptyData)
+        #expect(output?.response.url == httpResponse?.url)
+        #expect(output?.response.statusCode == httpResponse?.statusCode)
     }
 }
 
-private extension URLSessionHTTPClientTests {
-    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.protocolClasses = [URLProtocolStub.self]
-
-        let session = URLSession(configuration: configuration)
-        let sut = URLSessionHTTPClient(session: session)
-
-        trackForMemoryLeaks(session, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-
-        return sut
-    }
-
-    func expectRequest(at index: Int, toEqual expected: URLRequest, file: StaticString = #filePath, line: UInt = #line) {
-        let captured = URLProtocolStub.observedRequests[index]
-        XCTAssertEqual(captured.url, expected.url, file: file, line: line)
-        XCTAssertEqual(captured.allHTTPHeaderFields, expected.allHTTPHeaderFields, file: file, line: line)
-    }
-
-    func resultErrorFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) async -> Error? {
-        URLProtocolStub.stub(data: data, response: response, error: error)
-        let sut = makeSUT(file: file, line: line)
-        do {
-            let requestURL = makeURL()
-            let request = URLRequest(url: requestURL)
-            _ = try await sut.dispatch(request)
-            XCTFail("Expected failure but got success instead", file: file, line: line)
-            return nil
-        } catch {
-            return error
+extension URLSessionHTTPClientTests {
+    final class TestFixtures {
+        
+        private var sutTracker: MemoryLeakTracker<URLSessionHTTPClient>?
+        
+        deinit {
+            URLProtocolStub.removeStub()
+            sutTracker?.verify()
         }
-    }
-
-    func resultValuesFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) async -> (data: Data, response: HTTPURLResponse)? {
-        URLProtocolStub.stub(data: data, response: response, error: error)
-        let sut = makeSUT(file: file, line: line)
-        do {
-            let requestURL = makeURL()
-            let request = URLRequest(url: requestURL)
-            return try await sut.dispatch(request)
-        } catch {
-            XCTFail("Expected success but got failure instead", file: file, line: line)
-            return nil
+        
+        func makeSUT(sourceLocation: SourceLocation = #_sourceLocation) -> URLSessionHTTPClient {
+            let configuration = URLSessionConfiguration.ephemeral
+            configuration.protocolClasses = [URLProtocolStub.self]
+            
+            let session = URLSession(configuration: configuration)
+            let sut = URLSessionHTTPClient(session: session)
+            
+            sutTracker = .init(instance: sut, sourceLocation: sourceLocation)
+            
+            return sut
+        }
+        
+        func expectRequest(at index: Int, toEqual expected: URLRequest, sourceLocation: SourceLocation = #_sourceLocation) {
+            let captured = URLProtocolStub.observedRequests[index]
+            
+            #expect(captured.url == expected.url, sourceLocation: sourceLocation)
+            #expect(captured.allHTTPHeaderFields == expected.allHTTPHeaderFields, sourceLocation: sourceLocation)
+        }
+        
+        func resultErrorFor(data: Data?, response: URLResponse?, error: Error?, sourceLocation: SourceLocation = #_sourceLocation) async -> Error? {
+            URLProtocolStub.stub(data: data, response: response, error: error)
+            
+            let sut = makeSUT(sourceLocation: sourceLocation)
+            
+            do {
+                let request = URLRequest(url: Test.makeURL())
+                _ = try await sut.dispatch(request)
+                
+                Issue.record(
+                    "Expected failure but got success instead",
+                    sourceLocation: sourceLocation
+                )
+                
+                return nil
+            } catch {
+                return error
+            }
+        }
+        
+        func resultValuesFor(data: Data?, response: URLResponse?, error: Error?, sourceLocation: SourceLocation = #_sourceLocation) async -> (data: Data, response: HTTPURLResponse)? {
+            URLProtocolStub.stub(data: data, response: response, error: error)
+            
+            let sut = makeSUT(sourceLocation: sourceLocation)
+            
+            do {
+                let request = URLRequest(url: Test.makeURL())
+                return try await sut.dispatch(request)
+            } catch {
+                Issue.record(
+                    "Expected success but got failure instead",
+                    sourceLocation: sourceLocation
+                )
+                
+                return nil
+            }
         }
     }
 }
